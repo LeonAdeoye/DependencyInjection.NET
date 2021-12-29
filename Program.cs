bdb54750061c7845f023c02174aa357f666f43f7
LeonAdeoye <leon.adeoye@gmail.com>
2021-12-29 17:02:41 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("Hello, World!");
+
+static Task Main(string[] args) => CreateHostBuilder(args).Build().RunAsync();
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureServices((_, services) =>
+            services.AddHostedService<WorkerService>().AddScoped<IMessageWriter, MessageWriter>());
---
+++ WorkerService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace DependencyInjection
+{
+    internal class WorkerService : BackgroundService
+    {
+        private readonly IMessageWriter _messageWriter;
+
+        public WorkerService(IMessageWriter messageWriter) => _messageWriter = messageWriter;
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _messageWriter.Write($"Worker running at: {DateTimeOffset.Now}");
+                await Task.Delay(1000, stoppingToken);
+            }
+
+        }
+    }
+}
